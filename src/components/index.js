a9f86fe113c57fcca666f2b3246b074630df41e7
Jude <judemusyoki@gmail.com>
2023-02-11 16:54:41 +0100

    set up components & music player

---
+++ src/components/index.js
@@ -0,0 +1,25 @@
+import ArtistCard from './ArtistCard'
+import DetailsHeader from './DetailsHeader'
+import Error from './Error'
+import Loader from './Loader'
+import MusicPlayer from './MusicPlayer'
+import RelatedSongs from './RelatedSongs'
+import Searchbar from './Searchbar'
+import Sidebar from './Sidebar'
+import SongBar from './SongBar'
+import SongCard from './SongCard'
+import TopPlay from './TopPlay'
+
+export {
+  TopPlay,
+  Sidebar,
+  SongCard,
+  Searchbar,
+  ArtistCard,
+  DetailsHeader,
+  SongBar,
+  RelatedSongs,
+  MusicPlayer,
+  Loader,
+  Error,
+}
---
+++ src/pages/index.js
@@ -0,0 +1,17 @@
+import AroundYou from './AroundYou'
+import ArtistDetails from './ArtistDetails'
+import Discover from './Discover'
+import Search from './Search'
+import SongDetails from './SongDetails'
+import TopArtists from './TopArtists'
+import TopCharts from './TopCharts'
+
+export {
+  Discover,
+  Search,
+  TopArtists,
+  ArtistDetails,
+  SongDetails,
+  TopCharts,
+  AroundYou,
+}
