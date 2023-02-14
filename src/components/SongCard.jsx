710c7cf6431413b9e13de9b511e0a2aa29966ad1
Jude <judemusyoki@gmail.com>
2023-02-14 18:09:41 +0100

    add play/pause functionality

--- src/components/SongCard.jsx
+++ src/components/SongCard.jsx
@@ -1,5 +1,60 @@
-const SongCard = () => (
-  <div>SongCard</div>
-);
+import { useDispatch } from 'react-redux'
+import { Link } from 'react-router-dom'
 
-export default SongCard;
+import { playPause, setActiveSong } from '../redux/features/playerSlice'
+import PlayPause from './PlayPause'
+
+const SongCard = ({ song, isPlaying, activeSong, data, i }) => {
+  const dispatch = useDispatch()
+
+  const handlePauseClick = () => {
+    dispatch(playPause(false))
+  }
+  const handlePlayClick = () => {
+    dispatch(setActiveSong({ song, data, i }))
+    dispatch(playPause(true))
+  }
+
+  return (
+    <div className="flex flex-col w-[250px] p-4 bg-white/5 rounded-lg cursor-pointer">
+      <div className="relative w-full h-56 group">
+        <div
+          className={`absolute inset-0 justify-center items-center 
+      bg-black bg-opacity-50 group-hover:flex 
+      ${
+        activeSong?.title === song.title
+          ? 'flex bg-black bg-opacity-70'
+          : 'hidden'
+      }`}
+        >
+          <PlayPause
+            song={song}
+            isPlaying={isPlaying}
+            activeSong={activeSong}
+            handlePause={handlePauseClick}
+            handlePlay={handlePlayClick}
+          />
+        </div>
+        <img src={song.images?.coverart} alt="song_img" />
+      </div>
+      <div className="mt-4 flex flex-col">
+        <p className="font-semibold text-lg text-white truncate">
+          <Link to={`/songs/${song?.key}`}>{song.title}</Link>
+        </p>
+        <p className="text-sm truncate text-gray-300 mt-1">
+          <Link
+            to={
+              song.artists
+                ? `/artists/${song?.artists[0].adamid}`
+                : '/top-artists'
+            }
+          >
+            {song.subtitle}
+          </Link>
+        </p>
+      </div>
+    </div>
+  )
+}
+
+export default SongCard
--- src/pages/Discover.jsx
+++ src/pages/Discover.jsx
@@ -1,8 +1,12 @@
+import { useDispatch, useSelector } from 'react-redux'
+
 import { genres } from '../assets/constants'
 import { Error, Loader, SongCard } from '../components'
 import { useGetTopChartsQuery } from '../redux/services/shazamCore'
 
 const Discover = () => {
+  const dispatch = useDispatch()
+  const { activeSong, isPlaying } = useSelector((state) => state.player)
   const { data, isFetching, error } = useGetTopChartsQuery()
 
   if (isFetching) return <Loader title="Loading songs..." />
@@ -31,7 +35,14 @@ const Discover = () => {
 
       <div className="flex flex-wrap sm:justify-start justify-center gap-8">
         {data?.map((song, i) => (
-          <SongCard key={i} song={song} index={i} />
+          <SongCard
+            key={i}
+            song={song}
+            i={i}
+            isPlaying={isPlaying}
+            activeSong={activeSong}
+            data={data}
+          />
         ))}
       </div>
     </div>
